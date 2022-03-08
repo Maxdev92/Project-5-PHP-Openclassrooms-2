837aa2ce35e809cb1f99e8531476a53477500159
Maxime Schubas <maxime.schubas@gmail.com>
2022-03-08 18:48:48 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once(dirname(__DIR__).'/BLOG-MVC-MASTER/vendor/autoload.php');
+use App\Router;
+
+
+
+// require_once('controllers/Router.php');
+
+$router = new Router();
+
+$router->routeReq();
+
+
+
+
+ ?>
---
+++ src/Controllers/ControllerAccueil.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\ArticleManager;
+use App\Service\View;
+
+/**
+ *
+ */
+class ControllerAccueil
+{
+  private $_articleManager;
+  private $_view;
+
+  public function __construct()
+  {
+    if (isset($url) && count($url) > 1) {
+      throw new \Exception("Page introuvable", 1);
+
+    }
+    else {
+      $this->articles();
+    }
+  }
+
+  private function articles(){
+    $this->_articleManager = new ArticleManager();
+    $articles = $this->_articleManager->getArticles();
+    $this->_view = new View('Accueil');
+    $this->_view->generate(array('articles' => $articles));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
