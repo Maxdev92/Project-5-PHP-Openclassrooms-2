7312384d67f1390b1d3dc2ad8d45432516e88b7d
Maxime Schubas <maxime.schubas@gmail.com>
2022-03-15 21:46:23 +0100

    creation d'une classe (singleton) de connexion à la bdd

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Models;
 
-
+use App\DbConnect;
 
 abstract class Model
 {
@@ -9,28 +9,17 @@ abstract class Model
   private static $_bdd;
 
   //connexion a la bdd
-
-  private static function setBdd(){
-    self::$_bdd = new \PDO('mysql:host=localhost;dbname=php_libro_mvc;charset=utf8', 'root', '');
-
-    //on utilise les constantes de PDO pour gérer les erreurs
-    self::$_bdd->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
+  public function __construct(){
+    self::$_bdd = DbConnect::getInstance();
   }
 
-  //fonction de connexion par defaut a la bdd
-  protected function getBdd(){
-    if (self::$_bdd == null) {
-      self::setBdd();
-      return self::$_bdd;
-    }
-  }
 
   //creation de la methode
   //de récupération de liste d'elements
   //dans la bdd
 
   protected function getAll($table, $obj){
-    $this->getBdd();
+
     $var = [];
     $req = self::$_bdd->prepare('SELECT * FROM '.$table.' ORDER BY id desc');
     $req->execute();
@@ -48,9 +37,8 @@ abstract class Model
 
   }
 
-  protected function getOne($table, $obj, $id)
+  protected function getPost($table, $obj, $id)
   {
-    $this->getBdd();
     $var = [];
     $req = self::$_bdd->prepare("SELECT id, title, content, DATE_FORMAT(date, '%d/%m/%Y à %Hh%imin%ss') AS date FROM " .$table. " WHERE id = ?");
     $req->execute(array($id));
@@ -62,35 +50,33 @@ abstract class Model
     $req->closeCursor();
   }
 
-  protected function createOne($table, $obj)
+  protected function createPost($table, $obj)
   {
-    $this->getBdd();
-    $req = self::$_bdd->prepare("INSERT INTO ".$table." (title, content, date) VALUES (?, ?, ?)");
+    $req = self::$_bdd->prepare("INSERT INTO ".$table." (title, content, creation_date) VALUES (?, ?, ?)");
     $req->execute(array($_POST['title'], $_POST['content'], date("d.m.Y")));
 
     $req->closeCursor();
   }
 
+  protected function getCom($table, $obj, $id)
+  {
+    $var = [];
+    $req = self::$_bdd->prepare("SELECT id, author, content, DATE_FORMAT(date, '%d/%m/%Y à %Hh%imin%ss') AS date FROM " .$table. " WHERE id = ?");
+    $req->execute(array($id));
+    while ($data = $req->fetch(\PDO::FETCH_ASSOC)) {
+      $var[] = new $obj($data);
+    }
 
+    return $var;
+    $req->closeCursor();
+  }
 
+  protected function createCom($table, $obj)
+  {
+    $req = self::$_bdd->prepare("INSERT INTO ".$table." (post_id, content, author, creation_date) VALUES (?, ?, ?, ?)");
+    $req->execute(array($_POST['author'], $_POST['content'], date("d.m.Y")));
 
-
-
-
-
-
-
-
+    $req->closeCursor();
+  }
 
 }
-
-
-
-
-
-
-
-
-
-
- ?>
