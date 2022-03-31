9007bde486f2ade7615bdc11bd51071d6f5a7b53
Maxime Schubas <maxime.schubas@gmail.com>
2022-03-31 19:44:11 +0200

    Optimisation fonction register

--- src/Manager/RegisterManager.php
+++ src/Manager/RegisterManager.php
@@ -5,16 +5,22 @@ namespace App\Manager;
 class RegisterManager extends AbstractManager
 {
 
-
-  public function register($username, $email, $password)
+  /**
+   * fonction qui enregistre l'utilisateur en bdd
+   * @param string $username
+   * @param string $email
+   * @param string password
+   * @return bool
+   */
+  public function register(string $username,string $email,string $password): bool
   {
-  $hash = PASSWORD_DEFAULT;
-  $password = password_hash($password, $hash);
+    $hash = PASSWORD_DEFAULT;
+    $password = password_hash($password, $hash);
     $req = self::$_bdd->prepare
-    ("INSERT INTO user(username, email, password, role, created_at) values('?, ?, ?, ?, ?')");  
-    $req->execute($username, $email, $hash, 'user', date("Y.d.m"));
+    ("INSERT INTO user(username, email, password, role, created_at) values(?, ?, ?, ?, ?)");  
+    $result = $req->execute([$username, $email, $hash, 'user', date("Y-m-d")]);
     $req->closeCursor();
-  
+    return $result;
   }
 
 
